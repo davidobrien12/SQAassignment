8e123570196a548381b46c781b1be20f8b65e24a
David O'Brien <C15426428@mydit.ie>
2019-04-12 21:48:19 +0100

    filling in example classes with code

--- src/unitTesting/CalculatorTest.java
+++ src/unitTesting/CalculatorTest.java
@@ -1,5 +1,33 @@
 package unitTesting;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculatorTest {
+	
+	@Test
+	public void testAdd() {
+		Calculator c = new Calculator(10,15);
+		assertEquals(25, c.add());
+	}
+
+	@Test
+	public void testSubtract() {
+		Calculator c = new Calculator(10,15);
+		assertEquals(-5, c.subtract());
+	}
+
+	@Test
+	public void testMultiply() {
+		Calculator c = new Calculator(10,15);
+		assertEquals(150, c.multiply());
+	}
+
+	@Test
+	public void testDivide() {
+		Calculator c = new Calculator(10,2);
+		assertEquals(5, c.divide());
+	}
 
 }
